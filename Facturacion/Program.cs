8b9826aafbc90680bffb997f5710f57a58f81e37
Jose Matos <josematosdiaz22@gmail.com>
2023-04-16 13:01:05 -0400

    Listo el archivo de texto de base de datos para articulos

--- Facturacion/Program.cs
+++ Facturacion/Program.cs
@@ -2,53 +2,88 @@
 
 bool cancel = false;
 int idArt, cantidad;
+string nombre;
+const string articulosSuper = "articulos.txt";
 
 List<Articulo> listaArt = new List<Articulo>();
 Articulo objArticulo;
 Factura objFactura = new Factura();
 
-CrearArticulos(72.99, "Leche", 1);
-CrearArticulos(8, "Huevos", 2);
-CrearArticulos(81.99, "Pan", 3);
-CrearArticulos(238.99, "Jamon", 4);
-CrearArticulos(183.99, "Queso", 5);
-CrearArticulos(5, "Guineos", 6);
-CrearArticulos(123.99, "Jugo", 7);
-CrearArticulos(89.00, "Refresco", 8);  
-CrearArticulos(7, "Manzanas", 9);
-CrearArticulos(92, "Piña", 10);
-
 //Inicio del programa
 
+LeerArticulos();
 Console.WriteLine("SISTEMA DE FACTURACION: \n");
 
 while (cancel == false)
 {
     Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
-    Console.WriteLine("Elija el articulo que desea agregar a su compra (1 - 10) (Si desea parar de agregar articulos seleccione un numero diferente de las opciones): ");
-    Console.WriteLine("[1]  - Leche \n[2]  - Huevos \n[3]  - Pan \n[4]  - Jamon \n[5]  - Queso \n[6]  - Guineos \n[7]  - Jugo \n[8]  - Refresco \n[9]  - Manzanas \n[10] - Piña");
+    Console.WriteLine($"Elija el articulo que desea agregar a su compra (1 - {listaArt.Count}) \n(Si desea parar de agregar articulos seleccione un numero diferente de las opciones): \n");
+
+    foreach (Articulo articulo in listaArt)
+    {
+        Console.WriteLine($"[{articulo.Id}] - {articulo.Nombre} || Precio: {articulo.Precio}$ \n");
+    }
+
     Console.WriteLine("---------------------------------------------------------------------------------------------------------------\n");
-        
+
     idArt = Convert.ToInt32(Console.ReadLine());
-        
-    if( idArt < 1 || idArt > listaArt.Count)
+
+    if (idArt < 1 || idArt > listaArt.Count)
     {
         cancel = true;
         break;
     }
 
-    objArticulo = listaArt.Find( articulo => articulo.Id == idArt );
+    objArticulo = listaArt.Find(articulo => articulo.Id == idArt);
 
-    Console.WriteLine($"ARTICULO: {objArticulo.Nombre}");
+    Console.WriteLine($"\nARTICULO: {objArticulo.Nombre}\n");
+    Console.WriteLine($"\nPRECIO: {objArticulo.Precio}\n");
     Console.WriteLine("Ingrese la cantidad que desea:\n");
     cantidad = Convert.ToInt32(Console.ReadLine());
     objArticulo.Cantidad = cantidad;
-
     objFactura.ArticulosFactura = objArticulo;
 
 }
 
+Console.Clear();
+Console.WriteLine("Introduzca el nombre del cliente: \n");
+nombre = Console.ReadLine();
+
 ImprimirFactura();
+Console.ReadLine();
+
+void LeerArticulos()
+{
+    bool cierre = false;
+    int i = 1;
+    StreamReader articulosA = new StreamReader(articulosSuper);
+    while (!cierre)
+    {
+        string nombre;
+        double precio;
+        try
+        {
+            nombre = articulosA.ReadLine();
+            precio = Convert.ToDouble(articulosA.ReadLine());
+
+            if (nombre == null)
+            {
+                throw new Exception();
+            }
+        }
+        catch
+        {
+            cierre = false;
+            break;
+        }
+
+        CrearArticulos(precio, nombre, i);
+        i++;
+    }
+
+    articulosA.Close();
+
+}
 
 void CrearArticulos(double precio, string nombre, int id)
 {
@@ -58,9 +93,25 @@ void CrearArticulos(double precio, string nombre, int id)
 
 void ImprimirFactura()
 {
-    Console.WriteLine("A continuacion los objetos de su factura: ");
-    foreach (var articulo in objFactura.FacturaLista)
+    double precioFactura = 0;
+    DateTime fecha = DateTime.Now;
+
+    Console.Clear();
+    Console.WriteLine("*----------------------------------------*");
+    Console.WriteLine("       FACTURA PARA CONSUMIDOR FINAL      ");
+    Console.WriteLine("*----------------------------------------*");
+    Console.WriteLine("DESCRIPCION (cant.)   |              VALOR");
+    Console.WriteLine("");
+    foreach (Articulo articulo in objFactura.FacturaLista)
     {
-        Console.WriteLine($"{articulo.Nombre}: {articulo.Cantidad} === {articulo.Precio * articulo.Cantidad}");
+        var precioFinal = articulo.Precio * articulo.Cantidad;
+        precioFactura += precioFinal;
+        Console.WriteLine($"{articulo.Nombre}   : {articulo.Cantidad}                      {precioFinal:C}");
     }
+    Console.WriteLine("*----------------------------------------*");
+    Console.WriteLine($"TOTAL A PAGAR:                     {precioFactura:C}");
+    Console.WriteLine("*----------------------------------------*");
+    Console.WriteLine("");
+    Console.WriteLine($"Compra realizada: {fecha.ToShortDateString()} {fecha.ToShortTimeString()}");
+    Console.WriteLine($"Cliente: {nombre}");
 }
